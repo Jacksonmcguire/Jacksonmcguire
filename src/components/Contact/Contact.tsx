e5893c3933e231f0b0fb929e82e2f87bf9f2885b
Jackson McGuire <jaxmcguire@gmail.com>
2021-06-02 18:10:37 -0400

    Style project card

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,7 +1,7 @@
 import './Contact.css'
 export const Contact = () => {
   return (
-    <article className="contact">
+    <article className="contact" id="contact">
       <h3>Contact</h3>
       <p>Although I'm not currently looking for any new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I'll try my best to get back to you!</p>
       <button>Reach Out</button>
--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -3,27 +3,31 @@ import owen from './owen-wilson.png';
 import { FaReact, FaVuejs } from 'react-icons/fa'
 import { SiTypescript } from 'react-icons/si'
 import { FiGithub } from "react-icons/fi";
+import { GrHeroku } from 'react-icons/gr'
+type project = {
+  deploy?: string, 
+  desc?: string, 
+  github?: string, 
+  name?: string, 
+  tech?: string[],
+  gif?: string, 
+}
 
-export const Project = () => {
+export const Project = ({deploy, desc, github, name, tech, gif}: project) => {
+  console.log(gif)
   return (
     <div className="project-card">
-      <img src={owen} alt="owen wilson" />
-      <h4>Owen Wilson</h4>
-      <article>
-        <p>
-          Hello, this is my owen wilson bottle rocket project.
-          Here are some links and info about my tech stack.
+      <img src={gif} alt="project gif" />
+      <article className="project-info">
+      <h4>{name}</h4>
+        <p className="description">
+          {desc}
         </p>
+        <a href={deploy}><GrHeroku className="github"></GrHeroku></a>
+        <a href={github}><FiGithub className="github"></FiGithub></a>
         <ul>
-          <br />
-        <FaReact></FaReact>
-        <br />
-        <FaVuejs></FaVuejs>
-        <br />
-        <SiTypescript></SiTypescript>
+          {tech?.map(name => <p>{name}</p>)}
         </ul>
-        <FiGithub className="github"></FiGithub>
-        <a href="google.com">Deploy Link</a>
       </article>
     </div>
   )
