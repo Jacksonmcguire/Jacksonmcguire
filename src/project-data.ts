71d37a3ef8f8eb5f67e63d259ad999fa99f5a9bf
Jackson McGuire <jaxmcguire@gmail.com>
2021-06-03 17:27:55 -0400

    Change structure of description data

--- src/project-data.ts
+++ src/project-data.ts
@@ -3,7 +3,11 @@ export const projectData = [
     name: 'Flick Finder',
     github: 'https://github.com/What-2-Watch/what-2-watch-fe',
     deploy: 'https://flickfinder2011.herokuapp.com/#/',
-    desc: 'Collaborative project built in 2 weeks with the help of a backend team. A Movie database that allows a user to search for movies to watch, users can get movie recommendations based on their past feedback on movies',
+    desc: {
+      problem: 'Lacking movie recommendations, Lacking movie recommendations, Lacking movie recommendations, Lacking movie recommendations, Lacking movie recommendations, Lacking movie recommendations, Lacking movie recommendations ',
+      audience: 'Turing students and junior web devs',
+      challenges: 'ill figure this out later'
+    },
     tech: ['Vue', 'Router', 'Cypress', 'SASS', 'CircleCI', 'Javascript'],
     gif: 'https://media.giphy.com/media/YLB9pdDKnEZMLHubjV/giphy.gif'
   },
@@ -11,7 +15,12 @@ export const projectData = [
     name: 'metApi',
     github: 'https://github.com/itsnameissteven/metAPI',
     deploy: 'https://metapi-app.herokuapp.com/',
-    desc: 'Collaborative project built in 1 week. A hub of public Api’s, that allows a developer to filter through Api’s to find only the ones that match their search parameters',
+    desc: {
+      problem: 'Too many apis to choose from',
+      audience: 'Turing students and junior web devs',
+      challenges: 'ill figure this out later'
+    },
+    // 'Collaborative project built in 1 week. A hub of public Api’s, that allows a developer to filter through Api’s to find only the ones that match their search parameters',
     tech: ['React', 'Router', 'Cypress', 'Typescript'],
     gif: 'https://user-images.githubusercontent.com/72054706/115272559-c35fc100-a0fb-11eb-84c3-a39e5b153d55.gif'
   },
@@ -19,8 +28,12 @@ export const projectData = [
     name: 'Rotten Turtle Tales',
     github: 'https://github.com/Shakikka/rotten-turtle-tales',
     deploy: 'https://rottenturtletales.herokuapp.com/',
-    desc: 'Collaborative project built in 2 weeks. A hub of new movies, that allows a user to browse New movies in a gallery of movie posters and drag their favorite movies from the New Movies section into the Favorites section',
-    tech: ['React', 'React DND', 'Cypress', 'Router'],
+    desc: {
+      problem: 'Lacking movie recommendations',
+      audience: 'Turing students and junior web devs',
+      challenges: 'ill figure this out later'
+
+    },    tech: ['React', 'React DND', 'Cypress', 'Router'],
     gif: 'https://media.giphy.com/media/HzLcVvt2ylZgdEFDgc/giphy.gif'
   },
   
