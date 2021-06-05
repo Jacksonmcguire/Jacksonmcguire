d8979b0beb45c5e463a6004dd35a8fdc396e2e84
Jackson McGuire <jaxmcguire@gmail.com>
2021-06-05 09:46:09 -0400

    Change project data

--- src/project-data.ts
+++ src/project-data.ts
@@ -4,11 +4,10 @@ export const projectData = [
     github: 'https://github.com/What-2-Watch/what-2-watch-fe',
     deploy: 'https://flickfinder2011.herokuapp.com/#/',
     desc: {
-      problem: 'Lacking movie recommendations, Lacking movie recommendations, Lacking movie recommendations, Lacking movie recommendations, Lacking movie recommendations, Lacking movie recommendations, Lacking movie recommendations ',
-      audience: 'Turing students and junior web devs',
-      challenges: 'ill figure this out later'
+      goal: 'With the help of a backend team, build an app that allows indecisive movie watchers to provide feedback on popular and searched movies. Based on their feedback and selected streaming platforms, generate movie recommendations to show them.',
+      challenges: 'Building an app with Vue(new to frontend team). Learning and following a Git Rebase workflow. Evolving project MVP, and intial documentation. The look and essential functionality of the app were in flux throughout the project.'
     },
-    tech: ['Vue', 'Router', 'Cypress', 'SASS', 'CircleCI', 'Javascript'],
+    tech: ['Vue', 'Router', 'Cypress', 'SASS', 'CircleCI', 'JavaScript'],
     gif: 'https://media.giphy.com/media/YLB9pdDKnEZMLHubjV/giphy.gif'
   },
   {
@@ -16,12 +15,10 @@ export const projectData = [
     github: 'https://github.com/itsnameissteven/metAPI',
     deploy: 'https://metapi-app.herokuapp.com/',
     desc: {
-      problem: 'Too many apis to choose from',
-      audience: 'Turing students and junior web devs',
-      challenges: 'ill figure this out later'
+      goal: 'Build an app that makes it easier for a developer seeking a public api by filtering by search and other helpful parameters in a clean and elegant way.',
+      challenges: 'Learning and utilizing Typescript with React.'
     },
-    // 'Collaborative project built in 1 week. A hub of public Api’s, that allows a developer to filter through Api’s to find only the ones that match their search parameters',
-    tech: ['React', 'Router', 'Cypress', 'Typescript'],
+    tech: ['React', 'Router', 'Cypress', 'TypeScript'],
     gif: 'https://user-images.githubusercontent.com/72054706/115272559-c35fc100-a0fb-11eb-84c3-a39e5b153d55.gif'
   },
   {
@@ -29,11 +26,10 @@ export const projectData = [
     github: 'https://github.com/Shakikka/rotten-turtle-tales',
     deploy: 'https://rottenturtletales.herokuapp.com/',
     desc: {
-      problem: 'Lacking movie recommendations',
-      audience: 'Turing students and junior web devs',
-      challenges: 'ill figure this out later'
-
-    },    tech: ['React', 'React DND', 'Cypress', 'Router'],
+      goal: 'Learn how to use React through building an app that allows a user to view a gallery of new movies, favorite movies they like, and see a movie details page after clicking on a poster.',
+      challenges: 'Implementing testing for drag and drop functionality proved much harder than expected and we were not able to fully test that with Cypress.'
+    },    
+    tech: ['React', 'React Beautiful DND', 'Cypress', 'Router'],
     gif: 'https://media.giphy.com/media/HzLcVvt2ylZgdEFDgc/giphy.gif'
   },
   
