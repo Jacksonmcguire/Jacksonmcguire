e5ce289ff822b1d86ae4232d8f6ae2a780de77ab
Jackson McGuire <jaxmcguire@gmail.com>
2021-06-07 16:42:02 -0400

    Change About

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,5 +1,5 @@
 import './About.css'
-import logo from './IMG_2156.png';
+import logo from './github.jpg';
 import { FaReact, FaVuejs } from 'react-icons/fa'
 import { SiTypescript } from 'react-icons/si'
 
@@ -10,16 +10,21 @@ export const About = () => {
       <article className="about-content">
       <div>
         <p>
-          Back In 2019 while backpacking through Australia, I met an array of "nomad" software engineers and became 
-        immediately enamored with the work, and the opportunities it provided. Software development for me is a way to 
-        exercise my strong collaboration, creativity, and resourcefulness. 
+        While backpacking through Australia in 2019, I met an array of "nomad" software 
+        <div className="jackson">
+          <img src={logo} alt="Jackson" />
+        </div>
+        engineers and became immediately enamored with the work and the opportunities it provided. Like trekking through rugged trails, the Turing School
+        of Software and Design has challenged me to push boundaries as a “not your 
+        average” developer.  For me, software development is a way to exercise strong 
+        collaboration, creativity, and resourcefulness.
         </p>
         <p>
-          Like trekking through rugged trails, the Turing school of Software and design is challenging and pushing me 
-        outside of my boundaries to become not just your average developer. My expertise is in building Javascript 
-        applications with the help of frameworks such as React and Vue. I have found that a sense of discovery and 
-        curiosity is paramount as a developer, I am always looking to add to my tech stack and to grow as an engineer 
-        through my peers and teammates. 
+        My expertise is in building Javascript applications with the help of frameworks
+        such as React and Vue. I have found that a sense of discovery and curiosity is 
+        paramount as a developer, I am always looking to add to my tech stack and to
+        grow as an engineer through my peers and teammates.
+
         </p>
         <ul>
         <label>Current technologies:</label> 
@@ -28,7 +33,6 @@ export const About = () => {
         <p>Typescript</p>
         </ul>
       </div>
-      <img src={logo} alt="Jackson" />
       </article>
     </section>
   )
